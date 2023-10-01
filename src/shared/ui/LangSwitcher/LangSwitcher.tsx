49de7b9d7b875cf1964f9d745470949833594e57
DaniilLogvinenko <daniillogvinenkospb@mail.ru>
2023-10-01 12:00:01 +0300

    fixed indent

--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -3,23 +3,23 @@ import { classNames } from "shared/lib/classNames/classNames";
 import Button, { ThemeButton } from "../Button/Button";
 
 interface LangSwitcherProps {
-  className?: string;
+    className?: string;
 }
 
 export const LangSwitcher = ({ className }: LangSwitcherProps) => {
-  const { t, i18n } = useTranslation();
+    const { t, i18n } = useTranslation();
 
-  const toggle = async () => {
-    i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru");
-  };
+    const toggle = async () => {
+        i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru");
+    };
 
-  return (
-    <Button
-      className={classNames("", {}, [className])}
-      theme={ThemeButton.CLEAR}
-      onClick={toggle}
-    >
-      {t("Язык")}
-    </Button>
-  );
+    return (
+        <Button
+            className={classNames("", {}, [className])}
+            theme={ThemeButton.CLEAR}
+            onClick={toggle}
+        >
+            {t("Язык")}
+        </Button>
+    );
 };
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -5,26 +5,26 @@ import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
 import classes from "./Navbar.module.scss";
 
 interface NavbarProps {
-  className?: string;
+    className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  return (
-    <div className={classNames(classes.Navbar, {}, [className])}>
-      <div className={classes.links}>
-        <AppLink
-          theme={AppLinkTheme.SECONDARY}
-          className={classes.mainLink}
-          to="/about"
-        >
-          {t("О сайте")}
-        </AppLink>
-        <AppLink theme={AppLinkTheme.SECONDARY} to="/">
-          {t("Главная")}
-        </AppLink>
-      </div>
-    </div>
-  );
+    return (
+        <div className={classNames(classes.Navbar, {}, [className])}>
+            <div className={classes.links}>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    className={classes.mainLink}
+                    to="/about"
+                >
+                    {t("О сайте")}
+                </AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/">
+                    {t("Главная")}
+                </AppLink>
+            </div>
+        </div>
+    );
 };
