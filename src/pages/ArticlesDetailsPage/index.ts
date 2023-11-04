7b0fef4427c0ae78a39830786b57af1b2358793e
DaniilLogvinenko <daniillogvinenkospb@mail.ru>
2023-11-04 11:00:25 +0300

    lesson 62 (deploy) v2

--- src/pages/ArticlesDetailsPage/index.ts
+++ src/pages/ArticlesDetailsPage/index.ts
@@ -1,4 +1,4 @@
 export { ArticlesDetailsPageAsync as ArticlesDetailsPage } from "./ui/ArticlesDetailsPage/ArticlesDetailsPage.async";
 export { ArticlesDetailsCommentsSchema } from "./model/types/ArticlesDetailsCommentsSchema";
-export { articleDetailsRecommendationsSchema } from "./model/types/articleDetailsRecommendationsSchema";
+export { articleDetailsRecommendationsSchema } from "./model/types/ArticleDetailsRecommendationsSchema";
 export { ArticleDetailsPageSchema } from "./model/types/index";
