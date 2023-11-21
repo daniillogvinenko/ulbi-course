3716f0311ef4d73b08bf609ec3cc7ba1457b151e
DaniilLogvinenko <daniillogvinenkospb@mail.ru>
2023-11-21 14:59:29 +0300

    lesson 85

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vitePluginSvgr from "vite-plugin-svgr";
+
+export default defineConfig({
+    plugins: [
+        react(),
+        vitePluginSvgr({
+            exportAsDefault: true,
+        }),
+    ],
+    resolve: {
+        alias: [{ find: "@", replacement: "/src" }],
+    },
+    define: {
+        __IS_DEV__: JSON.stringify(true),
+        __API__: JSON.stringify("http://localhost:8000"),
+        __PROJECT__: JSON.stringify("frontend"),
+    },
+});
